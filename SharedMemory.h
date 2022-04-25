1d5742f819e0e9f4d83b57d57a5618c052f7bc36
CCSUCola <63536836+CCSUCola@users.noreply.github.com>
2022-04-25 21:14:08 +0800

    Update SharedMemory.h

--- SharedMemory.h
+++ SharedMemory.h
@@ -1,17 +1,29 @@
 #include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include <string.h>
-#include <queue>
-#include <pthread.h>
 using namespace std;
-class MsgQueue
+class SharedMemory
 {
 public:
-    MsgQueue();
-    ~MsgQueue();
-    queue<string>que;
-    pthread_mutex_t mtx;
+    SharedMemory();
+    ~SharedMemory();
+    union semun  // 用于信号灯操作的共同体。
+    {
+        int val;
+        struct semid_ds *buf;
+        unsigned short *arry;
+    };
+    int SharedMemory_init(int key1, int size, int shmflg, int key2, int nsems, int semflg);
+    bool SharedMemory_Wait();
+    bool SharedMemory_Write(string& Message);
+    string SharedMemory_Read();
+    bool SharedMemory_Close();
+    int shmid; // 共享内存标识符
+    char *ptext=NULL;//指向共享内存地址
+    int  sem_id;  // 信号灯描述符
 
-    void MsgQueue_Init();
-    bool MsgQueue_Wait();
-    bool MsgQueue_Close();
 };
